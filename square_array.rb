dee32bd1858986310c8c2ae66e1414c633a98bae
Erin Stansbury <ekstansbury@gmail.com>
2020-01-04 03:32:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
   # your code here
-  count = 0
+
   array.each do |arr|
+    count = 0
     while count <= array.length-1
-    array[count]*array[count]
+    array[count] * array[count]
     count += 1
     end
   end
