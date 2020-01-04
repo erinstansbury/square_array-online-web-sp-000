3e05bdda0cb1027d449bbaa7715fbf78fe15e48e
Erin Stansbury <ekstansbury@gmail.com>
2020-01-04 03:41:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,8 +4,8 @@ def square_array(array)
   array.each do |arr|
     count = 0
     while count <= array.length-1
-    array[count] * array[count]
-    count += 1
+      array[count] * array[count]
+      count += 1
     end
   end
 end
